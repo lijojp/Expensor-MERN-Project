dd66b2632cf16d2e75a7a53aa898f9dbc19b445a
lijo J Palathingal <lijojpalathingal@gmail.com>
2023-01-02 10:28:27 +0530

    mongoDB connection created

---
+++ readme.md
@@ -0,0 +1,5 @@
+mongodb cluster name : bitfumes-mern
+username : bitfumes
+password : bitfumes123
+
+mongodb+srv://<username>:<password>@bitfumes-mern.mcu6ng2.mongodb.net/?retryWrites=true&w=majority
--- server/server.js
+++ server/server.js
@@ -1,10 +1,17 @@
 import express from 'express'
+import mongoose from 'mongoose';
 const app = express()
 const PORT = 4000
 
 app.get('/',(req,res)=>{
     res.send("hellow world")
 })
+
+await mongoose.connect(
+    'mongodb+srv://bitfumes:bitfumes123@bitfumes-mern.mcu6ng2.mongodb.net/?retryWrites=true&w=majority'
+    )
+console.log("mongodb connected")
+
 
 app.listen(PORT,()=>{
     console.log(`server running at http://localhost:${PORT}`)
